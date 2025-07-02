9c5eabc43beae0b304ad749a8985896caa5945be
sunny <mote82@paran.com>
2025-07-02 23:18:08 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { add, sub } from "./math.js";
+import randomColor from "randomcolor";
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+const color = randomColor();
+console.log(color);
